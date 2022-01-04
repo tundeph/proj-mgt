c2aa43b025b4086490a0331a6cb0a632f055931e
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-04 22:17:05 +0100

    1.0

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignedinLinks from "./SignedinLinks";
 
 const Navbar = () => {
   return (
     <nav className="nav wrapper grey darken-3">
-      <div className="container"></div>
-      <Link to="/" className="brand-logo">
-        ProjMgr
-      </Link>
+      <div className="container">
+        <Link to="/" className="brand-logo">
+          EzProjects
+        </Link>
+        <SignedinLinks />
+      </div>
     </nav>
   );
 };
